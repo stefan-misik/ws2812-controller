f5f09bc7d18d5fefb6db19362ab09bf62f3d99e7
Stefan Misik <stefan-misik@users.noreply.github.com>
2020-11-20 19:39:44 +0100

    [fw] Add event message definition

--- fw/system/common.hpp
+++ fw/system/common.hpp
@@ -10,17 +10,59 @@
 namespace system
 {
 
-enum class Message: uint8_t
-{
-    KEY_DOWN,
-    KEY_PRESS,
-    KEY_UP
-};
+/**
+ * @brief ID of a single tile used
+ */
+using TileId = uint8_t;
+
 
 enum class Key: char
 {
     ENTER = '5'
 };
+
+struct Rectangle
+{
+    uint8_t left;
+    uint8_t top;
+    uint8_t right;
+    uint8_t bottom;
+};
+
+struct Tile
+{
+    typedef int8_t (*TileProcedure)(uint8_t message_type, char * data);
+
+    Tile * next_tile;
+    TileId parent;
+    TileId first_child;
+    Rectangle position;
+
+    TileProcedure procedure;
+    void * opaque;
+};
+
+
+/**
+ * @brief Event message passed among the system components
+ */
+struct Message
+{
+    enum MessageType: uint8_t
+    {
+        KEY_DOWN,
+        KEY_PRESS,
+        KEY_UP,
+
+        USER = 128
+    };
+
+    TileId target;
+    uint8_t type;
+    char data[4];
+};
+
+static constexpr TileId INVALID_TILE = 255;
 
 
 }  // namespace system
